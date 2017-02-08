581d59dd511f187e3ae8b76986a221259bed8026
matthew-french <matthew.french@gmail.com>
2017-02-08 15:07:04 +0000

    project template setup

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+// Gulp Dependencies
+var gulp = require('gulp');
+var rename = require('gulp-rename');
+
+// Build Dependencies
+var browserify = require('gulp-browserify');
+var uglify = require('gulp-uglify');
+
+// Style Dependencies
+var less = require('gulp-less');
+var prefix = require('gulp-autoprefixer');
+var minifyCSS = require('gulp-minify-css');
+
+// Development Dependencies
+var jshint = require('gulp-jshint');
+
+// Test Dependencies
+var mochaPhantomjs = require('gulp-mocha-phantomjs');
